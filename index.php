ced4447f0363386cd86d448b5da648e2ab31640f
METEHAN ARSLAN <mthnnnarslan@gmail.com>
2024-03-30 20:59:54 +0300

    create this page

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+$kurs1_baslik = "Php Kursu";
+$kurs1_altBaslik = "Sıfırdan ileri seviye web geliştirme";
+$kurs1_resim = "php.jpg";
+$kurs1_yayinTarihi = "30.03.2024";
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <div class="container my-3">
+        <div class="card mb-3">
+            <div class="row">
+                <div class="col-3">
+                    <img src="img/php.jpg" alt="" class="img-fluid rounded-start">
+                </div>
+                <div class="col-9">
+                    <div class="card-body">
+                        <h5 class="card-title">Php Kursu</h5>
+                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus beatae minus quasi iure harum? Veniam
+                            vitae sapiente blanditiis ducimus sequi!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3">
+            <div class="row">
+                <div class="col-3">
+                    <img src="img/php.jpg" alt="" class="img-fluid rounded-start">
+                </div>
+                <div class="col-9">
+                    <div class="card-body">
+                        <h5 class="card-title">Php Kursu</h5>
+                        <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus beatae minus quasi iure harum? Veniam
+                            vitae sapiente blanditiis ducimus sequi!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
